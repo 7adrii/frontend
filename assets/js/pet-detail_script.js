cc209b1787861bf099295b777c5206aac8badcc2
LauraLG2000 <l.labartagonzalez@gmail.com>
2026-05-09 13:14:31 +0200

    Refactor pet detail page styles and enhance allergy section with dropdown options; improve pet data fetching logic

--- assets/js/pet-detail_script.js
+++ assets/js/pet-detail_script.js
@@ -6,20 +6,34 @@ window.addEventListener("DOMContentLoaded", () => {
   console.log(idPet);
 
   const urlPet = `http://localhost:8080/pets/${idPet}`;
-
+  const urlAllergies = `http://localhost:8080/allergies/pet/${idPet}`;
+  
   const getPetData = async () => {
     try {
-      const [pets] = await Promise.all([fetch(urlPet)]);
+      const pet = await fetch(urlPet);
+      const petData = await pet.json();
 
-      const petsData = await pets.json();
 
-      console.log(petsData);
+      const dniOwner = petData.data.owner_dni;
+      const urlOwner = `http://localhost:8080/owners/${dniOwner}`;
 
-      if (petsData.data) {
-        createPets(petsData.data);
-      }
+      const owner = await fetch(urlPet);
+      const ownerData = await owner.json();
+
+      const allergies = await fetch(urlAllergies);
+      const allergiesData = await allergies.json();
+
+      console.log(petData);
+      console.log(ownerData);
+      console.log(allergiesData);
+
+      /*if (petData.data) {
+        createPet(petData.data);
+      }*/
     } catch (error) {
       console.error(error);
     }
   };
+
+  getPetData();
 });
